61effb216a58694b20a7b4974fa2ed3d909e23c1
Glen Alloy <glenalloy8@gmail.com>
2024-05-01 12:56:47 +0530

    few changes in js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+$("#login").css({display:'block'});
+$("#dashboard-section").css({display: 'none'});
+$("#customer-section").css({display: 'none'});
+$("#item-section").css({display: 'none'});
+$("#place-order-section").css({display: 'none'});

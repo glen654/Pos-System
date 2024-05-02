b99f597ee70f20c3ee318036f11f92be3795808f
Glen Alloy <glenalloy8@gmail.com>
2024-05-02 11:06:12 +0530

    few changs in js file

--- js/script.js
+++ js/script.js
@@ -3,3 +3,8 @@ $("#dashboard-section").css({display: 'none'});
 $("#customer-section").css({display: 'none'});
 $("#item-section").css({display: 'none'});
 $("#place-order-section").css({display: 'none'});
+
+$("#btn-signIn").on('click',() =>{
+    $("#login").css({display:'none'});
+    $("#dashboard-section").css({display: 'block'});
+});

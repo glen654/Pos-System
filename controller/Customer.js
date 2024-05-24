89a8bef39ec054ea8efe607c433ed7ac99b5732e
Glen Alloy <glenalloy8@gmail.com>
2024-05-24 11:29:22 +0530

    few changes

--- controller/Customer.js
+++ controller/Customer.js
@@ -15,14 +15,23 @@ function loadTableCustomer(){
         $("#customer-table").append(record);
     });
 }
-$("#btn-save").on('click',() =>{
+
+$("#btn-customer-save").on('click',() =>{
+    console.log("button is clicked");
     var cusId = $("#customer-id").val();
     var cusName = $("#customer-name").val();
     var cusAddress = $("#customer-address").val();
     var cusTel = $("#customer-tel").val();
-    
+
+
+    console.log(cusId);
+    console.log(cusName);
+    console.log(cusAddress);
+    console.log(cusTel);
     let customer = new CustomerModel(cusId,cusName,cusAddress,cusTel);
     
     customers.push(customer);
     loadTableCustomer();
 });
+
+

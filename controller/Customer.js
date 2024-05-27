96fe3ebe18223fd689de699f5ddc1e39e7a05320
Glen Alloy <glenalloy8@gmail.com>
2024-05-27 19:48:51 +0530

    customer update done

--- controller/Customer.js
+++ controller/Customer.js
@@ -1,6 +1,8 @@
 import CustomerModel from "../model/CustomerModel.js";
 import {customers} from "../db/db.js"
 
+var recordIndex;
+
 function loadTableCustomer(){
     $("#customer-table").empty();
     
@@ -31,6 +33,38 @@ $("#customer-btn-save").on('click',() =>{
     
     customers.push(customer);
     loadTableCustomer();
+
+    $("#customer-btn-reset").click();
 });
 
+$("#customer-btn-update").on('click',() =>{
+    var cusId = $("#cus_id").val();
+    var cusName = $("#cus_name").val();
+    var cusAddress = $("#cus_address").val();
+    var cusTel = $("#cus_tel").val();
+    
+    let cusObject = {...customers[recordIndex]};
+    
+    cusObject.cusId = cusId;
+    cusObject.cusName = cusName;
+    cusObject.cusAddress = cusAddress;
+    cusObject.cusTel = cusTel;
+    
+    loadTableCustomer();
+    $("#customer-btn-reset").click();
+});
 
+$("#customer-table").on('click','tr',function (){
+    let index = $(this).index();
+    recordIndex = index;
+
+    let id = $(this).find(".customer-id-value").text();
+    let name = $(this).find(".customer-name-value").text();
+    let address = $(this).find(".customer-address-value").text();
+    let tel = $(this).find(".customer-tel-value").text();
+
+    $("#cus_id").val(id);
+    $("#cus_name").val(name);
+    $("#cus_address").val(address);
+    $("#cus_tel").val(tel);
+});

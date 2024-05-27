ab71bbafc6e75673c7ec4144f6f23d75fd1921c8
Glen Alloy <glenalloy8@gmail.com>
2024-05-27 19:26:54 +0530

    customer save and load table done

--- controller/Customer.js
+++ controller/Customer.js
@@ -1,33 +1,32 @@
 import CustomerModel from "../model/CustomerModel.js";
-/*import {customers} from "../db/db.js"*/
+import {customers} from "../db/db.js"
 
-var customers = [];
 function loadTableCustomer(){
     $("#customer-table").empty();
     
     customers.map((item,index) =>{
         var record = `<tr>
-            <td class="customer-id-value">${item.id}</td>
-            <td class="customer-name-value">${item.name}</td>
-            <td class="customer-address-value">${item.address}</td>
-            <td class="customer-tel-value">${item.tel}</td>
+            <td class="customer-id-value">${item.cusId}</td>
+            <td class="customer-name-value">${item.cusName}</td>
+            <td class="customer-address-value">${item.cusAddress}</td>
+            <td class="customer-tel-value">${item.cusTel}</td>
         <tr>`
         $("#customer-table").append(record);
     });
 }
 
-$("#btn-customer-save").on('click',() =>{
-    console.log("button is clicked");
-    var cusId = $("#customer-id").val();
-    var cusName = $("#customer-name").val();
-    var cusAddress = $("#customer-address").val();
-    var cusTel = $("#customer-tel").val();
+$("#customer-btn-save").on('click',() =>{
+    var cusId = $("#cus_id").val();
+    var cusName = $("#cus_name").val();
+    var cusAddress = $("#cus_address").val();
+    var cusTel = $("#cus_tel").val();
 
 
     console.log(cusId);
     console.log(cusName);
     console.log(cusAddress);
     console.log(cusTel);
+    
     let customer = new CustomerModel(cusId,cusName,cusAddress,cusTel);
     
     customers.push(customer);

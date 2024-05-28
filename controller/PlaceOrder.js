27da75430f8a4bd6635e4eb1f6c3a78b3162a826
Glen Alloy <glenalloy8@gmail.com>
2024-05-28 22:00:27 +0530

    few changes in place order js

--- controller/PlaceOrder.js
+++ controller/PlaceOrder.js
@@ -1,3 +1,16 @@
 import PlaceOrderModel from "../model/PlaceOrderModel.js";
-import {orders} from "../db/db.js"
+import {orders} from "../db/db.js";
+import {customers} from "../db/db.js";
+import {items} from "../db/db.js";
+
+
+function generateOrderId(){
+    const orderId = 'OR' + Date.now();
+    $("#order-label").val(orderId);
+}
+
+
+function loadCustomerIds(){
+
+}
 

80b354433b6c827bc5bc912981459bc9dd56eb6e
Glen Alloy <glenalloy8@gmail.com>
2024-05-29 11:17:38 +0530

    few changes in place order js

--- controller/PlaceOrder.js
+++ controller/PlaceOrder.js
@@ -1,19 +1,39 @@
 import PlaceOrderModel from "../model/PlaceOrderModel.js";
+import CustomerModel from "../model/CustomerModel.js";
 import {orders,customers,items} from "../db/db.js";
 
 
-function generateOrderId(){
-    const orderId = 'OR' + Date.now();
-    $("#order-label").val(orderId);
-}
+$(document).ready(()=>{
+    generateOrderId();
+    loadCustomerIds();
 
 
-function loadCustomerIds(){
-    customers.forEach(customer => {
-        $("#inputState").append(new Option(customer.cusId, customer.cusId));
+    function generateOrderId(){
+        const orderId = 'OR' + Date.now();
+        $("#order-label").val(orderId);
+    }
+
+
+    function loadCustomerIds(){
+        customers.forEach(customer => {
+            $("#inputState").append(new Option(customer.cusId, customer.cusId));
+        });
+    }
+
+    $("#inputState").on('change',function (){
+        const selectedCustomerId = $(this).val();
+        const selectedCustomer = customers.find(customer => customer.cusId === selectedCustomerId);
+
+        if(selectedCustomer){
+            $("#name-input").val(selectedCustomer.cusName);
+            $("#address-input").val(selectedCustomer.cusAddress);
+        }else{
+            $("#name-input").val('');
+            $("#address-input").val('');
+        }
     });
-}
+});
 
 
-generateOrderId();
-loadCustomerIds();
+
+
